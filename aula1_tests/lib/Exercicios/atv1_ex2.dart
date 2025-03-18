7989303a66b189cedc9790ad6010b96494269d2f
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-03-17 21:15:52 -0300

    adding ex atv1 (1 e 2) e começando rotas

---
+++ aula1_tests/lib/Exercicios/atv1_ex2.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Home Page")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Hello, World!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: const Text("A button")),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- aula1_tests/lib/main.dart
+++ aula1_tests/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(RandomApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class RandomApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Random App')),
+      body: Center(child: Text('Welcome to Random App')),
     );
   }
 }
